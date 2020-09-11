01e1b858a6671cb584c34952458b8325b1b1dfcd
miracle-halation <miraclehalation.9710@gmail.com>
2020-09-11 18:37:38 +0900

    createメソッドを記載

--- app/controllers/v1/todos_controller.rb
+++ app/controllers/v1/todos_controller.rb
@@ -1,2 +1,14 @@
 class V1::TodosController < ApplicationController
+	def create
+		todo = Todo.new(todo_params)
+		if todo.save
+			render json: todo, status: :created
+		else
+			render json: todo.errors, status: :unprocessable_entry
+		end
+	end
+
+	def destroy
+		
+	end
 end

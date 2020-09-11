4dd997418a19c3e23fda9a96d316b3a5d9e76b0c
miracle-halation <miraclehalation.9710@gmail.com>
2020-09-11 18:40:59 +0900

    usersコントローラーのindexアクションを記載

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,2 +1,13 @@
 class V1::UsersController < ApplicationController
+	def index
+		if params[:uid]
+			current_user = User.find_by(uid: params[:uid])
+			render json: current_user
+		else
+			users = User.all
+			render json: users
+		end
+	end
+
+	
 end

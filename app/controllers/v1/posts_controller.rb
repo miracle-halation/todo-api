5628e5d6b15676d3998fb69977cd998dbb4ecbe5
miracle-halation <miraclehalation.9710@gmail.com>
2020-10-01 14:33:11 +0900

    postsのルーティングを設定

--- app/controllers/v1/posts_controller.rb
+++ app/controllers/v1/posts_controller.rb
@@ -1,2 +1,13 @@
 class V1::PostsController < ApplicationController
+	def index
+	end
+
+	def create
+	end
+
+	private
+	
+	def post_params
+		params.require(:post).permit(:title, :content, :user_id)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   namespace :v1 do
     resources :todos, only: [:create, :destroy]
     resources :users, only: [:index, :create]
+    resources :posts, only: [:index, :create]
   end
 end
